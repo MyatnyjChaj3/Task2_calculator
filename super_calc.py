c2d51174b93cfd7591e737f7030d0ac4379efc6b
asfqx <polinabrunina25@gmail.com>
2024-12-06 21:16:25 +0300

    Add 5-8 functions

--- super_calc.py
+++ super_calc.py
@@ -2,4 +2,25 @@ import math
 
 
 class Calculator:
-    pass
+    def remainder(a, b):
+        try:
+            return a % b 
+        except ZeroDivisionError:
+            print('На ноль делить нельзя!')
+
+    def sinus(a):
+        if a <= 360 or a >= 0:
+            a = a * math.pi() // 180
+            return math.sin(a)
+        else:
+            print('Число должно находиться в диапазоне от 0 до 360')
+    
+    def cosinus(a,b):
+        if a <= 360 or a >= 0:
+            a = a * math.pi() // 180
+            return math.cos(a)
+        else:
+            print('Число должно находиться в диапазоне от 0 до 360')
+    def pow(a,b):
+        return a**b
+
